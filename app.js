afdec45d20d3f035373dab66cbcc013f7f8f5cc4
Mr-Arsal <an24547@gmail.com>
2023-09-13 18:04:54 +0500

    Completed

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Sign Up 
 
- // Retrieve the input elements
  let firstName = document.getElementById('first-name');
  let lastName = document.getElementById('last-name');
  let signupEmail = document.getElementById('signup-email');
@@ -10,22 +9,23 @@
 
  // Load data from Local Storage when the page loads
  window.onload = function() {
-     firstName.value = localStorage.getItem('firstName') || '';
-     lastName.value = localStorage.getItem('lastName') || '';
-     signupEmail.value = localStorage.getItem('signupEmail') || '';
+     firstName.value = localStorage.getItem('first-name') || '';
+     lastName.value = localStorage.getItem('last-name') || '';
+     signupEmail.value = localStorage.getItem('signup-email') || '';
      password.value = localStorage.getItem('password') || '';
      address.value = localStorage.getItem('address') || '';
-     phoneNum.value = localStorage.getItem('phoneNum') || '';
+     phoneNum.value = localStorage.getItem('phone-num') || '';
  };
 
- // Store the values in Local Storage when the "Submit" button is clicked
- document.getElementById('submit-button').addEventListener('click', function() {
-     localStorage.setItem('firstName', firstName.value);
-     localStorage.setItem('lastName', lastName.value);
-     localStorage.setItem('signupEmail', signupEmail.value);
+ // Store the values in Local Storage
+ document.getElementById('submit-button').addEventListener('click', function(e) {
+    e.preventDefault()
+     localStorage.setItem('first-name', firstName.value);
+     localStorage.setItem('last-name', lastName.value);
+     localStorage.setItem('signup-email', signupEmail.value);
      localStorage.setItem('password', password.value);
      localStorage.setItem('address', address.value);
-     localStorage.setItem('phoneNum', phoneNum.value);
+     localStorage.setItem('phone-num', phoneNum.value);
      alert('Data saved to Local Storage.');
  });
 

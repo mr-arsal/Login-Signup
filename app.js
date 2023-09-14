c39469fff8a483a6eaa12e8ad27a93e7d5c38bad
Mr-Arsal <an24547@gmail.com>
2023-09-14 11:58:17 +0500

    Data Stored to local storage

--- app.js
+++ app.js
@@ -27,27 +27,27 @@ function validateEmail() {
 // ----------------------------------------------------------
 
 // Sign Up Page
-function store(e) {
+function store() {
+    document.getElementById("form-sign-up").addEventListener("submit", (e) => {
+        e.preventDefault()
+        const firstName = document.getElementById('first-name').value;
+        const lastName = document.getElementById('last-name').value;
+        const signupEmail = document.getElementById('signup-email').value;
+        const passwords = document.getElementById('password').value;
+        const address = document.getElementById('address').value;
+        const phoneNum = document.getElementById('phone-num').value;
 
-    const firstName = document.getElementById('first-name').value;
-    const lastName = document.getElementById('last-name').value;
-    const signupEmail = document.getElementById('signup-email').value;
-    const passwords = document.getElementById('password').value;
-    const address = document.getElementById('address').value;
-    const phoneNum = document.getElementById('phone-num').value;
-
-    const formData = {
-        firstName,
-        lastName,
-        signupEmail,
-        passwords,
-        address,
-        phoneNum
-    };
-    localStorage.setItem('formData', JSON.stringify(formData));
-    alert('Form data has been saved to local storage.');
-
-    e.preventDefault()
+        const formData = {
+            firstName,
+            lastName,
+            signupEmail,
+            passwords,
+            address,
+            phoneNum,
+        };
+        localStorage.setItem('formData', JSON.stringify(formData));
+        alert('Form data has been saved to local storage.');
+    })
 };
 
 
